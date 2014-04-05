0f39448a73a657ddf72dca6367cbda404918d217
baked <travp624@gmail.com>
2014-04-04 22:15:10 -0500

    Notification custom background setting not setting
    When setting a custom background only the gallery app would work
    this fixes that and sets the selected image no matter the app used to
    pick it. Big thanks to Roman Birg for this fix that he submitted to
    my commit on cm's gerrit.
    
    Signed-off-by: baked <travp624@gmail.com>

--- src/com/android/settings/baked/NotificationDrawer.java
+++ src/com/android/settings/baked/NotificationDrawer.java
@@ -22,9 +22,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.preference.ColorPickerPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -43,7 +45,8 @@ import com.android.settings.baked.SeekBarPreference;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class NotificationDrawer extends SettingsPreferenceFragment implements
@@ -55,6 +58,7 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     private static final String PANEL_BACKGROUND_STYLE = "panel_background_style";
     private static final String PANEL_WALLPAPER_ALPHA = "panel_wallpaper_alpha";
     private static final String PANEL_VIEW_BACKGROUND_COLOR_FILL = "panel_view_background_color_fill";
+    private static final String PANEL_BACKGROUND_FILE_NAME = "notifwallpaper";
 
     private static final int REQUEST_PICK_WALLPAPER = 201;
     private static final int COLOR_FILL = 0;
@@ -66,8 +70,6 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     private ListPreference mPanelBackground;
     private SeekBarPreference mWallpaperAlpha;
 
-    private File wallpaperImage;
-    private File wallpaperTemporary;
     private PreferenceCategory mPanelViewBackground;
 
     private int seekbarProgress;
@@ -85,9 +87,6 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
         mPanelBackground = (ListPreference) findPreference(PANEL_BACKGROUND_STYLE);
         mWallpaperAlpha = (SeekBarPreference) findPreference(PANEL_WALLPAPER_ALPHA);
         mPanelColorFill = (ColorPickerPreference) findPreference(PANEL_VIEW_BACKGROUND_COLOR_FILL);
-
-        wallpaperImage = new File(getActivity().getFilesDir()+"/notifwallpaper");
-        wallpaperTemporary = new File(getActivity().getCacheDir()+"/notifwallpaper.tmp");
     }
 
     @Override
@@ -178,23 +177,45 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_PICK_WALLPAPER) {
             if (resultCode == Activity.RESULT_OK) {
-                if (wallpaperTemporary.exists()) {
-                    wallpaperTemporary.renameTo(wallpaperImage);
+                Uri uri = null;
+                if (data != null) {
+                    uri = data.getData();
+                    FileOutputStream out = null;
+                    try {
+                        Bitmap wallpaper = getBitmapFromUri(uri);
+                        out = getActivity().openFileOutput(PANEL_BACKGROUND_FILE_NAME,
+                                Context.MODE_WORLD_READABLE);
+                        wallpaper.compress(Bitmap.CompressFormat.JPEG, 85, out);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if(out != null) {
+                            try {
+                                out.close();
+                            } catch (IOException e) {
+                            }
+                        }
+                    }
+                    Toast.makeText(getActivity(), getResources().getString(R.string.
+                            background_result_successful), Toast.LENGTH_LONG).show();
+                    Settings.System.putInt(getContentResolver(),
+                            Settings.System.PANEL_BACKGROUND_STYLE, 1);
+                    updateVisiblePreferences();
                 }
-                wallpaperImage.setReadable(true, false);
-                Toast.makeText(getActivity(), getResources().getString(R.string.
-                        background_result_successful), Toast.LENGTH_LONG).show();
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.PANEL_BACKGROUND_STYLE, 1);
-                updateVisiblePreferences();
             } else {
-                if (wallpaperTemporary.exists()) {
-                    wallpaperTemporary.delete();
-                }
                 Toast.makeText(getActivity(), getResources().getString(R.string.
                         background_result_not_successful), Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
     }
 
     private boolean handleBackgroundSelection(int index) {
@@ -239,12 +260,8 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
             intent.putExtra("aspectY", isPortrait ? height : width);
 
             try {
-                wallpaperTemporary.createNewFile();
-                wallpaperTemporary.setWritable(true, false);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(wallpaperTemporary));
                 intent.putExtra("return-data", false);
                 getActivity().startActivityFromFragment(this, intent, REQUEST_PICK_WALLPAPER);
-            } catch (IOException e) {
             } catch (ActivityNotFoundException e) {
             }
         } else if (index == DEFAULT) {
