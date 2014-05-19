aac4608dbdfaedec56316c5f08ab406a3cd861b3
baked <travp624@gmail.com>
2014-05-19 02:02:56 -0500

    Settings : Add hooks for device specific features
    Add support for :
    
    Devices with bluetooth input peripherals
    Devices with screen gesture support
    Devices with a back touchpad
    
    Conflicts:
    	res/xml/language_settings.xml
    	src/com/android/settings/cyanogenmod/ButtonSettings.java
    	src/com/android/settings/cyanogenmod/SystemUiSettings.java
    	src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
    
    Change-Id: Ia4835c69a8ba8fb76597e0ab0540f8ef1ed6e322

--- src/com/android/settings/baked/ButtonSettings.java
+++ src/com/android/settings/baked/ButtonSettings.java
@@ -53,6 +53,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight";
     private static final String KEY_SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
     private static final String KEY_VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+    private static final String KEY_BLUETOOTH_INPUT_SETTINGS = "bluetooth_input_settings";
     private static final String DISABLE_NAV_KEYS = "disable_nav_keys";
 
     private static final String CATEGORY_HOME = "home_key";
@@ -274,6 +275,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         if (!backlight.isButtonSupported() && !backlight.isKeyboardSupported()) {
             prefScreen.removePreference(backlight);
         }
+
+        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
+                getPreferenceScreen(), KEY_BLUETOOTH_INPUT_SETTINGS);
     }
 
     private ListPreference initActionList(String key, int value) {
